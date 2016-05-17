dd39855c5d9ad4ed06e9526ed298a60c9d035a57
firestorm451 <mzwick1684@hotmail.com>
2016-05-17 16:15:32 -0400

    Seeding for populating locations, materials, and machines working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Machine.create! ([
+  { :name => "Granulator 1" }, { :name => "Granulator 2"}, { :name => "Granulator 3"}, { :name => "Color 1"},
+  { :name => "Color 3"}, { :name "Wet 1"}, { :name "Wet 2"}, { :name "Wet 3"}, { :name "Extruder"}
+  ])
+
+Location.create! ([
+  { :name => "S1" }, { :name => "S2" }, { :name => "S3" }, { :name => "S4" }, { :name => "S5" },
+  { :name => "H1" }, { :name => "H2" }, { :name => "H3" }, { :name => "H4" }, { :name => "H5" },
+  { :name => "R1" }, { :name => "R2" }, { :name => "R3" }, { :name => "R4" }, { :name => "R5" },
+  { :name => "X1" }, { :name => "X2" }, { :name => "X3" }, { :name => "X4" }, { :name => "X5" }
+  ])
+
+Material.create! ([
+  { :name => "PP" }, { :name => "PE" }, { :name => "HIPS" }, { :name => "Fines" }, { :name => "Trash" }
+  ])
