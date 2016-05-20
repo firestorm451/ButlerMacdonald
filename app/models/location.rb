91ee92437771112e0151ccc2cfbe3d4ab3917a4c
firestorm451 <mzwick1684@hotmail.com>
2016-05-20 15:17:29 -0400

    Changed location minimum

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,6 @@
 class Location < ActiveRecord::Base
   has_many :boxes
-  validates_length_of :name, within: 2..3
+  validates_length_of :name, within: 2..6
   validates_presence_of :name
   validates_format_of :name, :with => /\A[a-zA-Z0-9]+\Z/
 end
