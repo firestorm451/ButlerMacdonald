50d02d5017a7394e56c8897a1cddce08b66066bd
firestorm451 <mzwick1684@hotmail.com>
2016-05-20 14:01:55 -0400

    Fixed nil bug

--- app/models/job_machine.rb
+++ app/models/job_machine.rb
@@ -4,8 +4,8 @@ class JobMachine < ActiveRecord::Base
   belongs_to :user
   has_many :input_boxes, class_name: Box, foreign_key: "input_id"
   has_many :output_boxes, class_name: Box, foreign_key: "output_id"
-  validates :step_number, uniqueness: {scope: :job_id}
-  validates_numericality_of :step_number, :greater_than_or_equal_to => 0
+  validates :step_number, uniqueness: {scope: :job_id}, allow_nil: true
+  validates_numericality_of :step_number, :greater_than_or_equal_to => 0, allow_nil: true
 
   def sum_of_input_boxes
     input_boxes.sum(:material_weight)
